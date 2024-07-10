4dd13d56ff2bca07a46188b9b3f0afeeb8b64ae9
Sugetha Kalyanaraman <sugethakch@arista.com>
2024-07-10 10:50:14 -0700

    CI: Update pre-commit versions (#695)
    * CI: updating min python version to 3.11
    
    * added pre-commit yaml file
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * added pylint ignore
    
    * make CI pass again
    
    * adding ignore for ansible-test sanity
    
    * make CI happy again!
    
    * hardcoding ansible-lint to keep CI happy
    
    * downgraded python version in matrix
    
    * pinning ansible-lint
    
    * CI happy: removed comments
    
    * CI happy: downgrade to 3.9
    
    * downgrading pylint version to 3.1.1
    
    * fixing pylint errors
    
    * upgrade python version to 3.10 in ansible-lint
    
    * partial ansible-lint commit
    
    * partial2: ansible-lint fix
    
    * partial2: ansible-lint fix
    
    * partial3: ansible-lint fix
    
    * partial4: ansible-lint fix
    
    * partial5: ansible-lint fix
    
    * moved yammlint inside the collection
    
    * changed yamllint rules
    
    * Run python unit tests only
    
    * excluding molecule/ and examples/
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fixed roles/configlet_sync
    
    * fix dhcp role
    
    * ansible-lint fix
    
    * Reverting to generic info in inventory
    
    * fixing typos
    
    * created new targets for running pytest
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- ansible_collections/arista/cvp/plugins/module_utils/validate_tools.py
+++ ansible_collections/arista/cvp/plugins/module_utils/validate_tools.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-# Copyright (c) 2023 Arista Networks, Inc.
+# Copyright (c) 2023-2024 Arista Networks, Inc.
 # Use of this source code is governed by the Apache License 2.0
 # that can be found in the LICENSE file.
 # coding: utf-8 -*-
-#
+# pylint: disable=unknown-option-value
 
 
 from __future__ import (absolute_import, division, print_function)
